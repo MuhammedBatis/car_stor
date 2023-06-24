ad24f9676796ecefa9b84f56b07dabdb4ab1dbb2
MuhammedBatis <132782410+MuhammedBatis@users.noreply.github.com>
2023-06-24 22:50:38 +0300

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+  'name': 'Car pro',
+  'version': '1.0',
+  'category': 'Accounting/Accounting',
+  'author': 'mohamed batis',
+  'website': "",
+  'company': 'mohamed batis',
+  'maintainer': 'mohamed batis',
+  'summary': 'Record Car Details',
+  "description": "",
+  'depends': ['base'],
+   'data': [
+      'security/car_pro.csv',
+      'views/car_pro_views.xml',
+  ],
+  'installable':True,
+  'application':True,
+  'license':'LGPL-3',
+ 
+}
